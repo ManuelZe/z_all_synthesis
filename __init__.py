9c1b0e08fc936c180bd7486f02230b324f7a9077
ManuelZe <lilianmanuelze@yahoo.com>
2025-12-04 15:09:02 +0100

    Ajoute de la synthèse du nombre de patients par rapport à un tarifaire 2

--- __init__.py
+++ __init__.py
@@ -12,9 +12,10 @@ def register():
     Pool.register(
         z_all_synthesis.Classement_Assurance_vente,
         generate_report.Elements_Actualisations,
-        z_all_synthesis.Produits_Sur_Peride,
+        z_all_synthesis.Produits_Sur_Periode,
         z_all_synthesis.Metriques,
         z_all_synthesis.Validation_Services,
+        z_all_synthesis.Nombre_Patients_Par_Tarifaire,
         module='z_all_synthesis', type_='model')
     Pool.register(
         generate_report.GenerateResultsReports,
